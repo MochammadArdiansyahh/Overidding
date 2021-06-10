0cd749d2662e6c43d03eed186ce0f04a7225e092
unknown <MochammadArdiansyahh>
2021-06-10 12:39:54 +0700

    berhasil

---
+++ src/overidding/Overidding.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overidding;
+
+/**
+ *
+ * @author Moch Ardiansyah
+ */
+public class Overidding {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        kedua io = new kedua();
+        io.bio("Nama Saya ");
+        
+        ketiga data = new ketiga();
+        data.bio("Saya dari kelas ");
+    }
+    
+}
---
+++ src/overidding/ketiga.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overidding;
+
+/**
+ *
+ * @author Moch Ardiansyah
+ */
+public class ketiga extends kedua{
+    String kelas;
+    
+    void bio (String kelas){
+        this.kelas = kelas;
+        System.out.println(kelas+"X RPL 2");
+    }    
+}
